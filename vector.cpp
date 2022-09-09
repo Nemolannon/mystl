ff69f0d4e2c535de39a44c9b2b74cea2ced8f0ec
nemolannon <awed87@mail.ru>
2022-09-09 14:16:22 +0400

    09092022 1416

--- vector.cpp
+++ vector.cpp
@@ -29,19 +29,17 @@ void vector<T>::inflate()
         // Типичное решение, которое часто встречается в интернете:
         // pStash = new T[nSize];
         // кажется ошибочным, во-первых потому что в этом случае для каждого
-        // созданного элемента T автоматически будет вызван абсолютно не нужный(а иногда и недоступный) в данном случае конструктор по умолчанию;
+        // созданного элемента T автоматически будет вызван не нужный(а иногда и недоступный) в данном случае конструктор по умолчанию;
         // во-вторых, копирование элементов в этот уже инициализированный массив будет возможно только побитово, например с помощью оператора "=":
         // pStash[i] = pOldStash[i];
         // соответственно, необходимый вызов копирующего конструктора для каждого копируемого элемента становится недоступным
-        // Поэтому выделение из кучи неотформатированной области, её принудительное форматирование и последующее размещение в эой облати
-        // копий существующих объектов при помощи оператора placement new кажется единственно верным решением.
-        for(int f(0); f < nCounter; ++f)
+        // Поэтому единственно верным решением кажется выделение из кучи неотформатированной области нужного размера,
+        // её принудительное форматирование(static_cast<>) и последующее размещение в эой облати
+        // копий существующих объектов при помощи оператора placement new.
+        for(T *receiver(pStash), *source(pOldStash), *bound(pOldStash + nCounter); source < bound; ++receiver, ++source)
         {
-            new(&(pStash[f])) T(pOldStash[f]);
-        }
-        for(int f(0); f < nCounter; ++f)
-        {
-            pOldStash[f].~T();  // В данном случае также необходим принудительный вызов деструктора для каждого элемента, что тоже выглядит не очень изящно, но другого способа я не нашел
+            new(receiver) T(*source);
+            source->~T();  // В данном случае также необходим принудительный вызов деструктора для каждого элемента, что тоже выглядит не очень изящно, но другого способа я не нашел
         }
         ::operator delete(pOldStash);   // Освобождаем память, которая была занята только что скопированным контейнером
 }
@@ -60,17 +58,50 @@ void vector<T>::pop_back()
     pStash[--nCounter].~T();
 }
 
-/*template<class T>
+template<class T>
 typename vector<T>::iterator vector<T>::insert(const_iterator pos, const T& val)
 {
-    if(pos == end()) { push_back(val); return back(); }
-    if(nCount == nSize) inflate();
-    iterator temp = back();
-    iterator dest = end();
-}*/
+    if(nCounter < nSize)// если место в контейнере ещё есть
+    {
+        T *receiver(end()), *source(&back());
+        for(; receiver > pos; --receiver, --source)
+        {
+            new(receiver) T(*source);
+            source->~T();
+        }
+
+        new(receiver) T(*pos);
+    }
+    else
+    {
+         nSize *= 2; // Увеличиваем размер
+        // Если превышен лимит, бросаем исключение
+        if(nSize >= MAX_SIZE) throw std::length_error("Exceeded the maximum size of the vector. MAX_SIZE is " + std::to_string(MAX_SIZE) + ", " + std::to_string(nSize) + " requested");
+        // Вопрос: если произошел resize, то итератор pos, как и вообще все сохраненные итераторы, становятся недействительными... Как быть?..
+        T* pOldStash = pStash;  // Сохраняем указатель
+        pStash = static_cast<T*>(::operator new(nSize * TYPE_SIZE));// Выделяем память под контейнер нового размера
+        T *receiver(pStash), *source(pOldStash);
+        for( ; source < npos; ++receiver, ++source)
+        {
+            new(receiver) T(*source);
+            source->~T();
+        }
+
+        new(receiver++) T(val);
+
+        for(T *bound(pOldStash + nCounter); source < bound; ++receiver, ++bound)
+        {
+            new(receiver) T(*source);
+            source->~T();
+        }
+
+        ::operator delete(pOldStash);   // Освобождаем память, которая была занята только что скопированным контейнером
+    }
+    ++nCounter;
+}
 
 template<class T>
-T& vector<T>::operator[](size_type n)
+typename vector<T>::reference vector<T>::operator[](size_type n)
 {
     return pStash[n];
 }
