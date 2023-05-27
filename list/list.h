1ae42b6950a4db7c871426c9bb5ce0ce5200428f
nemolannon <awed87@mail.ru>
2023-05-27 19:16:12 +0400

    list types

--- list/list.h
+++ list/list.h
@@ -1,1 +1,24 @@
 #pragma once
+
+namespace my{
+
+template<class T>
+class list
+{
+    public:
+
+    using value_type        = T;
+    using size_type         = std::size_t;
+    using reference 	    = value_type&
+    using const_reference   = const value_type&
+    using pointer           = value_type*;
+    using const_pointer 	= condt T*;
+    //using iterator;
+    //using const_iterator;
+    //using reverse_iterator;
+    //using const_reverse_iterator;
+
+    
+};
+
+};
